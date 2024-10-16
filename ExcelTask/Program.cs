f513b11d29f8189e67a3bd658189beb0fd81de1a
TE-199 <TE-199@DESKTOP-F0P79A1>
2024-10-16 11:28:08 +0530

    Add project files.

---
+++ ExcelTask/Program.cs
@@ -0,0 +1,42 @@
+﻿using ClosedXML.Excel;
+
+using var workbook = new XLWorkbook(@"C:\Book.xlsx");
+var backupData = workbook.Worksheet("BackUP Data ");
+var currentData = workbook.Worksheet("Current Data ");
+
+int rowNum = 2;
+
+Dictionary<string, string> dictionary = [];
+
+while (backupData.Cell("A" + rowNum).Value.IsBlank == false)
+{
+	string userId = backupData.Cell("A" + rowNum).Value.GetText();
+	string passwordHash = backupData.Cell("C" + rowNum).Value.GetText();
+
+	dictionary.Add(userId, passwordHash);
+	rowNum++;
+}
+
+rowNum = 2;
+int numberOfChangedPassword = 0;
+
+while (currentData.Cell("A" + rowNum).Value.IsBlank == false)
+{
+	string userId = currentData.Cell("A" + rowNum).Value.GetText();
+	string currentPassword = currentData.Cell("C" + rowNum).Value.GetText();
+
+
+	if (dictionary.ContainsKey(userId) && dictionary[userId].Equals(currentPassword) == false)
+	{
+		XLCellValue name = currentData.Cell("D" + rowNum).Value;
+
+		Console.WriteLine(rowNum + " " + name);
+		Console.WriteLine(dictionary[userId]);
+		Console.WriteLine(currentPassword);
+		Console.WriteLine("--------------------------------------------------");
+		numberOfChangedPassword++;
+	}
+
+	rowNum++;
+}
+Console.WriteLine("Number of users with changed password: " + numberOfChangedPassword);
